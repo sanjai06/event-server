3b46d999613e6d6e6b4e4d835e3e10b3e7315970
sanjai06 <nsksanjai@gmail.com>
2024-01-20 01:37:21 +0530

    new update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface HomepageCarousel extends Schema.Component {
+  collectionName: 'components_homepage_carousels';
+  info: {
+    displayName: 'Carousel';
+  };
+  attributes: {
+    imageslider: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'homepage.carousel': HomepageCarousel;
+    }
+  }
 }
